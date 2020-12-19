e7283a2333c474fbb4d95e40075260bdc09b7d7e
Amrathesh <48640575+amrathesh@users.noreply.github.com>
2020-12-19 12:26:03 +0530

    Add files via upload

---
+++ CCCV Charge and Balancing Simulink Model/passiveBalancing_ini.m
@@ -0,0 +1,17 @@
+%Passive Cell Balancing%
+%By Amrathesh & Shyamanth R H
+
+% Passive balancing parameters
+I_cc = 4;
+R = .2;
+R_bleed = 20;
+T_f = 600;
+V_th = 11.5;
+dV = 0.01;
+V_top = 4.1;
+
+
+P_gain = 100;
+I_gain = 10;
+
+
